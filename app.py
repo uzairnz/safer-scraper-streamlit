d8b1e7fe9e645a05ad3c603415d5efbb7bf7f588
uzairnz <uzairnz@hotmail.com>
2023-08-25 01:30:44 +0500

    Scraper version 1.0 common req with dot

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import json
 
 def main():
     st.title("Company Data Scraper")
-    usdot_code = int(st.number_input("Enter US DOT Code"))
+    usdot_code = st.number_input("Enter US DOT Code", value=0, step=1)
     if st.button("Fetch Company Details"):
         if usdot_code:
             company_details = scrape_company_data(usdot_code)
