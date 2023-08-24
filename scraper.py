5790876f3f8dc3b799f97526c1d8e84e53cd00b7
uzairnz <uzairnz@hotmail.com>
2023-08-25 03:24:30 +0500

    maybe fixed

--- scraper.py
+++ scraper.py
@@ -9,6 +9,7 @@ def scrape_company_data(usdot_code):
     company = client.get_by_usdot_number(usdot_code)
     json_string = company.to_json()
     company_dict = json.loads(json_string)
+
     return company_dict
 
 
