55d04b815acd57b999c1fd6e8695a811d47f5522
uzairnz <uzairnz@hotmail.com>
2023-08-25 01:33:07 +0500

    Scraper version 1.0 common req with dot

--- scraper.py
+++ scraper.py
@@ -6,6 +6,6 @@ from safer import CompanySnapshot
 def scrape_company_data(usdot_code):
     client = CompanySnapshot()
     company = client.get_by_usdot_number(usdot_code)
-    company_dict = json.loads(company)
+    # company_dict = json.loads(company)
 
-    return company_dict
+    return company
