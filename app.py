0876b876c8f3796b0b2d99a54e2e7838ee2a1731
uzairnz <uzairnz@hotmail.com>
2023-08-25 00:50:25 +0500

    Scraper version 1.0 common req with dot

--- app.py
+++ app.py
@@ -18,3 +18,25 @@ def main():
 
 if __name__ == "__main__":
     main()
+
+# Input range for MC/MX Number and also handling for null data. Skip null in this
+# Fetch in batches of 10k
+
+
+# Entity Type:	CARRIER  
+# Operating Status:	AUTHORIZED FOR Property	Out of Service Date:	None
+# Legal Name:	IRELAND SOLUTION INC 
+# DBA Name:	JT GLOBAL LOAD TRUCKING 
+# Physical Address:	8383 WILSHIRE BLVD SUITE 800
+# BEVERLY HILLS , CA   90211  
+# Phone:	(213) 278-7120  
+# Mailing Address:	8383 WILSHIRE BLVD SUITE 800
+# BEVERLY HILLS , CA   90211  
+# USDOT Number:	3423627 	State Carrier ID Number:	 
+# MC/MX/FF Number(s):	MC-1107367
+#  	DUNS Number:	-- 
+# Power Units:	1 	Drivers:	1 
+# MCS-150 Form Date:	07/27/2022 	MCS-150 Mileage (Year):	600 (2021) 
+
+# Other information for this carrier
+# SMS Results (Carrier Registration) -----> Carrier registration details ------> Email
