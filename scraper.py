16e29dfa0ec12f5edfcc1633b5b96ee2354f96bd
uzairnz <uzairnz@hotmail.com>
2023-08-25 02:33:43 +0500

    Scraper version 1.0 common req with dot

--- scraper.py
+++ scraper.py
@@ -20,14 +20,14 @@ def scrape_email_from_url(usdot_code):
     if response.status_code == 200:
         soup = BeautifulSoup(response.content, 'html.parser')
 
-        label_tag = soup.find('label', text='Email:')
-        if label_tag:
-            email_tag = label_tag.find_next('span', {'class': 'dat'})
-            if email_tag:
-                return email_tag.text.strip()
-            else:
-                return "Email not found on the page"
-        else:
-            return "Label 'Email:' not found on the page"
+        li_elements = soup.find_all('li')
+        for li_element in li_elements:
+            label_tag = li_element.find('label')
+            if label_tag and 'Email' in label_tag.get_text():
+                email_tag = li_element.find('span', {'class': 'dat'})
+                if email_tag:
+                    return email_tag.get_text().strip()
+        
+        return "Email not found on the page"
     else:
         return "Unable to fetch the webpage"
